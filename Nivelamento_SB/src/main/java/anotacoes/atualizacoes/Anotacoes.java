dc0f394eebd0f59b473e146bb532cf923b79471d
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-11-08 19:26:33 -0300

    Criação do data hora, Log e as aplicações: [aula 4 @Autowaed, @Component, @Component, @RequiredArgsConstructor, @Log4j2]

--- Nivelamento_SB/src/main/java/anotacoes/atualizacoes/Anotacoes.java
+++ Nivelamento_SB/src/main/java/anotacoes/atualizacoes/Anotacoes.java
@@ -1,6 +1,7 @@
+/*package anotacoes.atualizacoes;
+public class Anotacoes {
 package projetos.spring.boot.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,18 @@ import projetos.spring.boot.util.DateUtil;
 
 import java.time.LocalDateTime;
 import java.util.List;
-//ENDPOINT
+
 
 @RestController//ANOTAÇÃO O RETORNO QUE TEREMOS NESTA CLASSE SERÃO APENAS STRINGS(TODOS OS METODOS IRAM CHAMR O @RESPONSEBODY)
 @RequestMapping("anime")//FAZ QUE O ENDIPOIT TODOS ACESSEM ATRAVES DO localhost:8080/anime/list //A APLICAÇÃO @RestController IRÁ PROCURAR O ENDPOINT PARA LISTAR OS NOMES CRIADOS, PARA ISSO DEVE USAR O
 @Log4j2
-@RequiredArgsConstructor//ESTA ANOTAÇÃO CRIA UM CONSTRUTOR COM TODOS OS ATRIBUTOS RELACIONADOA A CLASSE QUE VOCE TEM, DEVE-SE ACRESCENTAR "final" DEPOIS DO private. pode usar também o @AllArgsConstructor sem ter que usar o "final"
 public class AnimeController {
-    //localhost:8080/anime/list
-    private final DateUtil dateUtil;
+    @Autowired//NÃO É O RECOMENDADO, É INJEÇÃO DE DEPENDENCIA VIA CAMPO DA CLASSE PARA SUBSITUIR O CONSTRUTOR - PARA FUNCIONAR, DEVE IR NA CLASSE QUE RELACIONADA E FAZER A APLICAÇÃO @Componnet, @Service ou @Repository
+    private DateUtil dateUtil;
     @GetMapping(path = "list")
     public List<Anime> list() {//CRIAR UM MÉTODO QUE IRÁ RETORNAR UMA LISTA DE ANIMES
         log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return List.of(new Anime("007"), new Anime("CVZ"), new Anime("Liga da Bagaça"));//INSTANCIOU A CLASSE ANIME PARA CRIAR A LISTA, E INSERE OS NOMES
     }
 }
+}*/
--- Nivelamento_SB/src/main/java/projetos/spring/boot/ApplicationStart.java
+++ Nivelamento_SB/src/main/java/projetos/spring/boot/ApplicationStart.java
@@ -1,13 +1,13 @@
-package projetos.spring.boot.start;
+package projetos.spring.boot;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-/*NESTA CLASSE É ESTRUTURADO toddo programa java*/
+//NESTA CLASSE É ESTRUTURADO toddo programa java
 
 @EnableAutoConfiguration//RESPONSÁVEL POR INICIAR AS APLICAÇÕES
-@ComponentScan(basePackages = "projetos.spring.boot")//É COLOCADO NO PARAMETRO O NOME DO PACOTE QUE DESEJA ACESSAR E CONTROLAR
+@ComponentScan//NESTA ETAPA A CLASSE ApplicationStart FOI MOVIDA DO PACOTE LOCAL"START" E FOI MOVIDA PARA O PACOTE PRINCIPAL "projetos.spring.boot", POR ESSE MOTIVO, NÃO PRECISA COLOCAR O END DO PACOTE COMO PARAMETRO. LOGO ESSA APLICAÇÃO IRÁ SCANNEAR O PACOTE EM QUE ELE SE ENCONTRA
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);//foi chamado um método pra identificar que essa classe é frameWork rodando Spring
