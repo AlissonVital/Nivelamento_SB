d39ba37363dcc9e07f3a9eb68d63c398d8a24e0e
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-11-09 15:23:20 -0300

    Arquivo criado no site https://start.spring.io/

---
+++ src/test/java/projetos/spring/boot/EstudoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
